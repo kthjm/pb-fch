ee244755a1eff77493a1389cf610b0e8e87e3b39
koto hajime <toxictoxer@gmail.com>
2017-12-02 18:27:05 +0900

    Update index.js

--- test/.hub/index.js
+++ test/.hub/index.js
@@ -12,12 +12,12 @@ describe(`hub:Hubs`, () => {
     test()
     test({ limit: 10 })
 
-    function test(opts={}){
+    function test(opts = {}) {
       const cb = sinon.stub().returns(cbValue)
       const hub = new Straight(total, cb, opts)
 
       const array = [...loop(hub)]
-      array.forEach((value) => assert.deepStrictEqual(value, cbValue))
+      array.forEach(value => assert.deepStrictEqual(value, cbValue))
 
       const limit = opts.limit || 20
       cb.args.forEach((arg, index) => {
@@ -30,42 +30,47 @@ describe(`hub:Hubs`, () => {
 
   it(`Random`, () => {
     const Random = rewire(`../../src/.hub/Random.js`).default
+    test()
+    test({ limit: 50 })
 
-    const cb = sinon.stub().returns(cbValue)
-    const hub = new Random(total, cb)
+    function test(opts = {}){
+      const cb = sinon.stub().returns(cbValue)
+      const hub = new Random(total, cb, opts)
 
-    const array = [...loop(hub)]
-    array.forEach((value) => assert.deepStrictEqual(value, cbValue))
+      const array = [...loop(hub)]
+      array.forEach(value => assert.deepStrictEqual(value, cbValue))
 
-    const offsets = [...hub._store.values()].sort((a, b) => a - b)
-    assert.equal(offsets.length, total)
-    assert.equal(offsets[0], 0)
-    assert.equal(offsets[offsets.length - 1], total - 1)
+      const offsets = [...hub._store.values()].sort((a, b) => a - b)
+      assert.equal(offsets.length, total)
+      assert.equal(offsets[0], 0)
+      assert.equal(offsets[offsets.length - 1], total - 1)
+    }
   })
 })
 
 describe(`hub:locals`, () => {
-
-  it(`twentyGenerator`, () => {
-    const twentyGenerator = rewire(`../../src/.hub/Random.js`).__get__('twentyGenerator')
+  it(`generateFromTo`, () => {
+    const generateFromTo = rewire(`../../src/.hub/Random.js`).__get__(
+      'generateFromTo'
+    )
 
     const offset = 100
-    const array = [...twentyGenerator(offset)]
+    const limit = 50
+    const array = [...generateFromTo(offset, limit)]
     assert.equal(array[0], offset)
-    assert.equal(array.length, 20)
+    assert.equal(array.length, limit)
   })
-
 })
 
 describe(`hub:throws`, () => {
   const modules = rewire(`../../src/.hub`)
-  const hubKeys = ["Straight", "Random"]
+  const hubKeys = ['Straight', 'Random']
 
   const first = 1
   const second = () => {}
 
   it(`first argument`, () => {
-    hubKeys.forEach((key) => throws(modules[key]))
+    hubKeys.forEach(key => throws(modules[key]))
 
     function throws(Hub) {
       const unNumbers = [undefined, null, true, 'string', {}, [], () => {}]
@@ -79,7 +84,7 @@ describe(`hub:throws`, () => {
   })
 
   it(`second argument`, () => {
-    hubKeys.forEach((key) => throws(modules[key]))
+    hubKeys.forEach(key => throws(modules[key]))
 
     function throws(Hub) {
       const unFunctions = [undefined, null, true, 'string', 1, {}, []]
